82abc2bd96d77d9a1170e4bd82ba71d9b81eeb05
102660231 <48194706+102660231@users.noreply.github.com>
2019-04-17 18:48:01 +1000

    add files

---
+++ nested_q3.cs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nested_q3.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 4;
+            int nextline = 0;
+            int line = 0;
+            while (nextline <= rows)
+            {
+                line = 0;
+                while (line <= nextline)
+                {
+                    Console.Write("*");
+                    line++;
+                }
+                Console.WriteLine("\n");
+                nextline++;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
